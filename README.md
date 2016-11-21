000b972c9bd4c6d06b52ae2ba9dffcea0ecbdb56
Roman Vasilev <s91630277@gmail.com>
2016-11-22 00:08:37 +0300

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-# running
+# sigh-proc
 sigh plugin for running processes (e.g. express server)
 
-https://github.com/sighjs/sigh-process
+This plugin is designed to run processes that run indefinitely. The first time the plugin instance receives an event it starts the configured process, on subsequent events it kills the running process and starts another. It forwards all input events but delays them until the process has output at least one line to standard output. In `watch` mode the plugin is a no-op.
+
+## Why?
+[sigh-process](https://github.com/sighjs/sigh-process) uses `proc.kill()` to kill process
+but it does not kill process immediately. On OS Windows sometimes error happens `EADDRINUSE`.
+This module uses [tree-kill](https://www.npmjs.com/package/tree-kill) module to kill process.
+
+## Example
+
+`npm install --save-dev sigh-proc` then add something like this to your `sigh.js`:
+```javascript
+var proc;
+
+module.exports = function(pipelines) {
+  pipelines.run = [ proc('node server.js') ]
+}
+```
+
+This would run the process whenever a source file is compiled:
+```javascript
+var glob, babel, write, proc;
+
+module.exports = function(pipelines) {
+  pipelines.build = [
+    glob({ basePath: 'src' }, '**/*.js'),
+    babel(),
+    write('lib'),
+    proc('node lib/server.js'),
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var spawn = require('child_process').spawn;
+var Bacon = require('sigh-core').Bacon;
+var log = require('sigh-core').log;
+var treeKill = require('tree-kill');
+
+module.exports = function(op, cmd) {
+    var proc = null;
+    if (!op.watch) return op.stream;
+    var splitCmd = cmd.split(' ');
+
+    var kill = function(proc) {
+        return new Promise(function(resolve, reject) {
+            treeKill(proc.pid, 'SIGKILL', function(err) {
+                if (err) return reject(err);
+                resolve();
+            });
+        });
+    };
+
+    var spawnProcess = function(resolve, reject) {
+        log('spawn process `%s`', cmd);
+        (proc ? kill(proc) : Promise.resolve())
+            .then(function() {
+                proc = spawn(splitCmd[0], splitCmd.slice(1), {
+                    stdio: [process.stdin, 'pipe', process.stderr]
+                });
+                proc.stdout.pipe(process.stdout);
+                proc.stdout.on('data', function(line) {
+                    if (resolve) {
+                        resolve();
+                        resolve = null;
+                    }
+                });
+            });
+    };
+
+    return op.stream.flatMap(function(events) {
+        return Bacon.fromPromise(new Promise(spawnProcess).then(function() {
+            return events;
+        }));
+    });
+};
